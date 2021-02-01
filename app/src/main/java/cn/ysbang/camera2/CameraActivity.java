2013df97ea8a663455020e70bd3f88fc017560c6
杨昌标 <yangchangbiao@ysbang.cn>
2021-02-01 17:43:38 +0800

    Initial commit

---
+++ app/src/main/java/cn/ysbang/camera2/CameraActivity.java
@@ -0,0 +1,19 @@
+package cn.ysbang.camera2;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.FragmentActivity;
+
+public class CameraActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.component_sign_camera_activity);
+        if (null == savedInstanceState) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.cl_component_sign_camera_activity_content, Camera2BasicFragment.newInstance())
+                    .commit();
+        }
+    }
+
+}
